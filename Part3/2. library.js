df62876d95ad8d5ba6cbe223d3cba35741897422
hyungeunShin <123938245+hyungeunShin@users.noreply.github.com>
2023-12-05 20:09:44 +0900

    2023-12-05

---
+++ Part3/2. library.js
@@ -0,0 +1,7 @@
+var a = 10;
+var b = 20;
+//하나만 내보낼 때 default
+//export default a;
+var c = 30;
+export {a, b};
+export default c;
---
+++ Part3/3. worker.js
@@ -0,0 +1,5 @@
+var i = 0;
+for(let j = 0; j < 10000000; j++) {
+    i++
+}
+postMessage(i);
